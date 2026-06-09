e010c57e8a7cee5a90a6acd5aa34dd674550f640
Maria Soledad Perozo <mperozo241000@gmail.com>
2026-06-09 09:38:10 +0200

    Config UTC

--- Attributes/NoFutureDateAttribute.cs
+++ Attributes/NoFutureDateAttribute.cs
@@ -7,32 +7,32 @@ namespace mi_tension_backend.Attributes
     /// Atributo de validación personalizado que asegura que una fecha no sea posterior a la actual.
     /// </summary>
     public class NoFutureDateAttribute : ValidationAttribute
+    {protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+{
+    if (value == null) return ValidationResult.Success;
+
+    DateTime dateToValidate;
+    if (value is DateOnly dateOnly)
     {
-        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
-        {
-            if (value == null) return ValidationResult.Success;
+        dateToValidate = dateOnly.ToDateTime(TimeOnly.MinValue);
+    }
+    else if (value is DateTime dateTime)
+    {
+        dateToValidate = dateTime.Kind == DateTimeKind.Utc
+            ? dateTime
+            : dateTime.ToUniversalTime();
+    }
+    else
+    {
+        return new ValidationResult("Formato de fecha no válido.");
+    }
 
-            DateTime dateToValidate;
+    if (dateToValidate > DateTime.UtcNow)
+    {
+        return new ValidationResult(ErrorMessage ?? "La fecha no puede ser futura.");
+    }
 
-            if (value is DateOnly dateOnly)
-            {
-                dateToValidate = dateOnly.ToDateTime(TimeOnly.MinValue);
-            }
-            else if (value is DateTime dateTime)
-            {
-                dateToValidate = dateTime;
-            }
-            else
-            {
-                return new ValidationResult("Formato de fecha no válido.");
-            }
-
-            if (dateToValidate > DateTime.Now)
-            {
-                return new ValidationResult(ErrorMessage ?? "La fecha no puede ser futura.");
-            }
-
-            return ValidationResult.Success;
-        }
+    return ValidationResult.Success;
+}
     }
 }
